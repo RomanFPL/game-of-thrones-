eb2ba24326f4e8963d8ad6ed342e62616d264827
Roman Franko <roma.franko99@gmail.com>
2021-07-16 23:52:40 +0300

    GotService refactoring with context

--- src/services/gotService.js
+++ src/services/gotService.js
@@ -1,9 +1,10 @@
+
 export default class GotService {
     constructor() {
         this._apiBase = 'https://www.anapioficeandfire.com/api';
     }
 
-    async getResource(url) {
+    getResource = async (url) => {
         const res = await fetch(`${this._apiBase}${url}`);
     
         if (!res.ok) {
@@ -13,33 +14,33 @@ export default class GotService {
         return await res.json();
     }
 
-    async getAllBooks() {
+    getAllBooks = async () => {
         const res = await this.getResource(`/books/`);
         return res.map(this._transformBook);
     }
     
-    async getBook(id) {
+    getBook = async (id) => {
         const book = await this.getResource(`/books/${id}/`);
         return this._transformBook(book);
     }
     
-    async getAllCharacters() {
-        const res = await this.getResource(`/characters?page=4&pageSize=10`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`/characters?page=5&pageSize=10`);
         return res.map(this._transformCharacter);
     }
     
-    async getCharacter(id) {
+    getCharacter = async (id) => {
         const character = await this.getResource(`/characters/${id}`);
         return this._transformCharacter(character);
     }
     
-    async getAllHouses() {
+    getAllHouses = async () => {
         const res = await this.getResource(`/houses/`);
         return res.map(this._transformHouse);
     }
     
-    async getHouse(id) {
-        const house = this.getResource(`/houses/${id}/`);
+    getHouse = async (id) => {
+        const house = await this.getResource(`/houses/${id}/`);
         return this._transformHouse(house);
     }
 
@@ -47,10 +48,10 @@ export default class GotService {
         if (data) {
             return data
         } else {
-            return 'N/A'
+            return 'no data :('
         }
-    }    
-    
+    }
+
     _extractId = (item) => {
         const idRegExp = /\/([0-9]*)$/;
         return item.url.match(idRegExp)[1];
