e99963d33910fdabd1f6909499c5ac1c3fb0a2e2
Roman Franko <roma.franko99@gmail.com>
2021-07-21 01:20:21 +0300

    Added dinamic book

--- src/components/pages/bookPage.js
+++ src/components/pages/bookPage.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import RowBlock from '../rowBlock/rowBlock';
 import ItemList from '../itemList';
-import ItemDetails, {Field} from '../itemDetails';
 import ErrorMessage from '../errorMessage';
 import GotService from '../../services/gotService';
+import { withRouter} from 'react-router';
 
-export default class BookPage extends Component {
+class bookPage extends Component {
     gotService = new GotService();
 
     state = {
-        selectedBook: null,
         error: false
     }
 
@@ -19,39 +17,21 @@ export default class BookPage extends Component {
         })
     }
 
-    onItemSelected = (id) => {
-        this.setState({
-            selectedBook: id
-        })
-    }
-    
-    
     
     render (){
         if (this.state.error) {
             return <ErrorMessage/>
         }
 
-        const itemList = (
+        return(
             <ItemList 
-            onItemSelected={this.onItemSelected}
+            onItemSelected={(itemId)=>{
+                this.props.history.push(itemId)
+            }}
             gotData={this.gotService.getAllBooks}
             renderItem={({name}) => name}/>
         )
-
-        const charDetails = (
-            <ItemDetails 
-            itemId={this.state.selectedBook}
-            gotData={this.gotService.getBook}
-            pageItem={"book"}           >
-                <Field field='numberOfPages' label='Pages'/>
-                <Field field='publisher' label='Published'/>
-                <Field field='released' label='Released'/>
-            </ItemDetails>
-        )
-
-        return(
-            <RowBlock left={itemList} right={charDetails}/>
-        )
     }
 }
+
+export default withRouter(bookPage);
